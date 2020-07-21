e0d8c1355ff498f7d68f911ce2c49ba8835fa013
tomgrenet <thomas.grenet@gmail.com>
2020-07-22 00:40:57 +0100

    Additional docs

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+///////////////////////////////////////////////////////////
+//1. BEFORE YOU START, CHECK CHILD BRANCHES IS USED ON GITHUB
+//2. mockup software: https://codingsans.com/blog/mockup-tools
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//////////////////////////////////////////////////////////
+
 function App() {
   return (
     <div className="App">
